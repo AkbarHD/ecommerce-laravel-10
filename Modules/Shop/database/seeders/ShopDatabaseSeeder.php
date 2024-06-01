8a41d3d6ccd543a9228ac7431287e80dce5cb97d
Akbar Hossam Delmiro <akbarhossam123@gmail.com>
2024-06-01 22:52:22 +0700

    install laravel module, setup database, & laravel ui

---
+++ Modules/Shop/database/seeders/ShopDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Shop\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class ShopDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
---
+++ Modules/Shop/resources/views/index.blade.php
@@ -0,0 +1,7 @@
+@extends('shop::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>Module: {!! config('shop.name') !!}</p>
+@endsection
